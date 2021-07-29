ea55d9d46383e372adaacec5e84824ef422b357f
EC2 Default User <ec2-user@ip-172-31-7-49.us-east-2.compute.internal>
2021-07-29 18:35:47 +0000

    adding local files

---
+++ local_file.rb
@@ -0,0 +1,44 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+    # gem 'aws-sdk-rekognition'
+    require 'aws-sdk-rekognition'
+    require 'base64'
+    require 'pry'
+    credentials = Aws::Credentials.new(
+       ENV['AWS_ACCESS_KEY_ID'],
+       ENV['AWS_SECRET_ACCESS_KEY']
+    )
+    client   = Aws::Rekognition::Client.new credentials: credentials
+    photo = 'wrestler.jpg'
+    path = File.expand_path(photo) # expand path relative to the current directory
+
+    file = File.read(path)
+    attrs = {
+      image: {
+        bytes: file
+      },
+      max_labels: 10
+    }
+    response = client.detect_labels attrs
+    puts "Detected labels for: #{photo}"
+    response.labels.each do |label|
+      puts "Label:      #{label.name}"
+      puts "Confidence: #{label.confidence}"
+      puts "Instances:"
+      label['instances'].each do |instance|
+        box = instance['bounding_box']
+        puts "  Bounding box:"
+        puts "    Top:        #{box.top}"
+        puts "    Left:       #{box.left}"
+        puts "    Width:      #{box.width}"
+        puts "    Height:     #{box.height}"
+        puts "  Confidence: #{instance.confidence}"
+      end
+      puts "Parents:"
+      label.parents.each do |parent|
+        puts "  #{parent.name}"
+      end
+      puts "------------"
+      puts ""
+    end
